d7c59e2032b09b9b7ab65ff5f47d449e6a80b9a7
MrHacker-X <70113933+MrHacker-X@users.noreply.github.com>
2020-12-15 13:19:59 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#/data/data/com.termux/files/usr/etc
+clear
+echo
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        Created by 'MrHacker-X'       |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        Instagram: @ hackerx30        |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo
+echo -e '\033[1;96m' "YouTube: https://m.youtube.com/MrHacker X/"
+echo -e '\033[1;92m'
+echo "Installing.................."
+echo
+apt install python -y
+pip install lolcat
+pkg install mpv -y
+echo
+rm /data/data/com.termux/files/usr/etc/bash.bashrc
+rm /data/data/com.termux/files/usr/etc/motd
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+cp -r lib /data/data/com.termux/files/usr/etc
+echo
+clear
+echo
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        Created by 'MrHacker-X'       |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        Instagram: @ hackerx30        |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo
+echo -e '\033[1;96m' "		Setup is finished."
+echo -e '\033[1;96m' "Restart your termux and see awesome look"
+echo
