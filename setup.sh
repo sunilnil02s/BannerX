6471eec49f45b5fc71f2fdf1f6d80617df6d3fcc
MrHacker-X <70113933+MrHacker-X@users.noreply.github.com>
2021-03-03 21:39:57 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,15 +4,16 @@ echo
 echo -e '\033[1;96m' "══════════════════════════════════════════"
 echo -e '\033[1;96m' " |        Created by 'MrHacker-X'       |"
 echo -e '\033[1;96m' "══════════════════════════════════════════"
-echo -e '\033[1;96m' " |        Instagram: @ hackerx30        |"
+echo -e '\033[1;96m' " |        Instagram: @ mrhacker.x       |"
 echo -e '\033[1;96m' "══════════════════════════════════════════"
 echo
-echo -e '\033[1;96m' "YouTube: https://m.youtube.com/MrHacker X/"
+echo -e '\033[1;96m' "YouTube: https://youtube.com/c/MrHackerXr"
 echo -e '\033[1;92m'
 echo "Installing.................."
 echo
 apt install python -y
 pip install lolcat
+pkg install figlet
 pkg install mpv -y
 echo
 rm /data/data/com.termux/files/usr/etc/bash.bashrc
@@ -25,9 +26,9 @@ echo
 echo -e '\033[1;96m' "══════════════════════════════════════════"
 echo -e '\033[1;96m' " |        Created by 'MrHacker-X'       |"
 echo -e '\033[1;96m' "══════════════════════════════════════════"
-echo -e '\033[1;96m' " |        Instagram: @ hackerx30        |"
+echo -e '\033[1;96m' " |        Instagram: @ mrhacker.x       |"
 echo -e '\033[1;96m' "══════════════════════════════════════════"
 echo
-echo -e '\033[1;96m' "		Setup is finished."
+figlet Finished | lolcat
 echo -e '\033[1;96m' "Restart your termux and see awesome look"
 echo
