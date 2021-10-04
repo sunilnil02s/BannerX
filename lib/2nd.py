7780fc75225acd5a8942d4dd425fec2602369c50
MrHacker-X <70113933+MrHacker-X@users.noreply.github.com>
2021-10-04 17:08:08 +0530

    Update 2nd.py

--- lib/2nd.py
+++ lib/2nd.py
@@ -1,6 +1,5 @@
 import os
 import time
-import sys
 # Set color
 G = '\033[1;32m' # Green
 
@@ -8,9 +7,9 @@ def delay_print(s):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(0.005)
+        time.sleep(0.003)
 delay_print
 delay_print (""+G+"╔════════════════════════════════════════╗  ""\n")
-delay_print (""+G+"║ Instagram: @ mrhacker.x                ║  ""\n")
+delay_print (""+G+"║ Instagram: @ hackerxmr                 ║  ""\n")
 delay_print (""+G+"║ Github: https://github.com/MrHacker-X/ ║  ""\n")
 delay_print (""+G+"╚════════════════════════════════════════╝  ""\n")
