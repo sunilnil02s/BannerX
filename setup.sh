4e491c423d6469b345cdd539ba717162296af44b
MrHacker X <70113933+MrHacker-X@users.noreply.github.com>
2023-03-08 01:25:20 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo
 echo -e '\033[1;96m' "══════════════════════════════════════════"
 echo -e '\033[1;96m' " |        Created by 'MrHacker-X'       |"
 echo -e '\033[1;96m' "══════════════════════════════════════════"
-echo -e '\033[1;96m' " |        Instagram: @ hackerxmr        |"
+echo -e '\033[1;96m' " |        Instagram: @ 0hacker_x0       |"
 echo -e '\033[1;96m' "══════════════════════════════════════════"
 echo
 echo -e '\033[1;96m' "YouTube: https://youtube.com/c/Sololex/"
@@ -28,7 +28,7 @@ echo
 echo -e '\033[1;96m' "══════════════════════════════════════════"
 echo -e '\033[1;96m' " |        Created by 'MrHacker-X'       |"
 echo -e '\033[1;96m' "══════════════════════════════════════════"
-echo -e '\033[1;96m' " |        Instagram: @ hackerxmr        |"
+echo -e '\033[1;96m' " |        Instagram: @ 0hacker_x0       |"
 echo -e '\033[1;96m' "══════════════════════════════════════════"
 echo
 figlet Finished | lolcat
