8b8929d3d0c35a94798281d9cbcea6c6e0280a3c
MrHacker X <70113933+MrHacker-X@users.noreply.github.com>
2023-03-08 01:23:05 +0530

    Update 2nd.py

--- lib/2nd.py
+++ lib/2nd.py
@@ -11,6 +11,6 @@ def delay_print(s):
         time.sleep(0.003)
 delay_print
 delay_print (""+G+"╔════════════════════════════════════════╗  ""\n")
-delay_print (""+G+"║ Instagram: @ hackerxmr                 ║  ""\n")
+delay_print (""+G+"║ Instagram: @ 0hacker_x0                ║  ""\n")
 delay_print (""+G+"║ Github: https://github.com/MrHacker-X/ ║  ""\n")
 delay_print (""+G+"╚════════════════════════════════════════╝  ""\n")
